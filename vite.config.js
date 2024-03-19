6c89594d0a0b6574e2a3b8de9e8313552f40839d
Juana Copello <copellojuana@gmail.com>
2024-03-19 18:49:47 -0300

    install gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vd-maqueta/",
+  base: "/vd_gh_pages/",
   plugins: [svelte()],
 });
